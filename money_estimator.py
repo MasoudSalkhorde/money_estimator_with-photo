560e516bfe01a028ac48498aef78bec627d30261
MasoudSalkhorde <63811249+MasoudSalkhorde@users.noreply.github.com>
2024-07-25 17:31:51 -0400

    Adding the sample image and openCV

--- money_estimator.py
+++ money_estimator.py
@@ -1,2 +1,9 @@
 import numpy as np
-print('hi')
+import cv2
+
+img = cv2.imread('sample-photo.png', cv2.IMREAD_GRAYSCALE)
+original_img = cv2.imread('sample-photo.png', 1)
+img = cv2.GaussianBlur(img, (5,5), 0)
+cv2.imshow('Detected Money', img)
+#cv2.waitkey(0)
+#cv2.destroyAllWindows()
